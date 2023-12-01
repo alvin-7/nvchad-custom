ea92c1b18250b08cd783377bad9b3a0177af2596
zye <zye@wooduan.com>
2023-12-01 12:59:36 +0800

    lsp config

--- mappings.lua
+++ mappings.lua
@@ -1,48 +1,6 @@
 ---@type MappingsTable
 local M = {}
 
-M.comment = {
-  plugin = true,
-
-  -- toggle comment in both modes
-  n = {
-    ["<C-/>"] = {
-      function()
-        require("Comment.api").toggle.linewise.current()
-      end,
-      "Toggle comment",
-    },
-  },
-
-  v = {
-    ["<C-/>"] = {
-      "<esc><cmd>lua require('comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-      "toggle comment",
-    },
-  },
-}
-
-M.nvterm = {
-  plugin = true,
-
-  n = {
-    ["<C-`>"] = {
-      function()
-        require("nvterm.terminal").toggle "horizontal"
-      end,
-      "Toggle horizontal term",
-    },
-  },
-  t = {
-    ["<C-`>"] = {
-      function()
-        require("nvterm.terminal").toggle "horizontal"
-      end,
-      "Toggle horizontal term",
-    },
-  }
-}
-
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
@@ -53,7 +11,5 @@ M.general = {
 }
 
 -- more keybinds!
-require("core.utils").load_mappings("comment")
-require("core.utils").load_mappings("nvterm")
 
 return M
